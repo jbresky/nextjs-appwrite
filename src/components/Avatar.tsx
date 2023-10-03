7d3756ed64e7dd975edd7febf3ac6ec6e7559162
jbresky <joaquin_bresky@hotmail.com>
2023-10-03 17:42:00 -0300

    app components

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,16 @@
+type Props = {
+    img: string;
+    alt?: string;
+}
+
+const Avatar: React.FC<Props> = ({ img, alt }) => {
+    return (
+        <div className="rounded-full overflow-hidden w-full pt-[100%] relative">
+            <div className="absolute inset-0">
+                <img src={img} alt={alt || img} />
+            </div>
+        </div>
+    )
+}
+
+export default Avatar
